741992f9a9b266982f27f90efd9bebd9a14a4a3c
baloo <juniorloo1998@gmail.com>
2020-04-23 01:34:17 +0200

    Model change array to private

--- src/Luhn.java
+++ src/Luhn.java
@@ -1,5 +1,3 @@
-package Controller;
-
 public class Luhn {
 
     //Klar
--- src/View/CustomerView.java
+++
@@ -1,9 +0,0 @@
-package View;
-
-public class CustomerView {
-    public void printCustomerDetails(String firstName, String lastName, String ssn){
-        System.out.println("First name: " + firstName);
-        System.out.println("Last name: " + lastName);
-        System.out.println( "Social security number: "  + ssn);
-    }
-}
